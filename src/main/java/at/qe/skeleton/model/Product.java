ec2ba23e0caf88c1459ad8665a41636272458ea8
Matti Sebastian Fischbach <matti.fischbach@student.uibk.ac.at>
2025-12-08 21:14:38 +0100

    feat: added product entity

---
+++ src/main/java/at/qe/skeleton/model/Product.java
@@ -0,0 +1,149 @@
+package at.qe.skeleton.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.domain.Persistable;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+/**
+ * Entity representing products.
+ */
+@Entity
+public class Product implements Persistable<Long>, Serializable, Comparable<Product> {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private double price;
+    @Column(nullable = false)
+    private int stock;
+
+    @Column(nullable = true)
+    private double discount;
+    @Column(nullable = true)
+    private String shortDescription;
+    @Column(nullable = true)
+    private String description;
+
+    // it seems that storing the actual image in the database is not a viable option for a high
+    // performance application like a webshop. You would want to store the images in a Cloud Object
+    // Storage system (AWS S3) and pair this with a Content Delivery Network (AWS Cloudfront).
+    @Column(nullable = true)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
+
+    // TODO: Reviews
+    // @OneToMany
+    // private Set<Reviews> reviews;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.getId());
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Product)) {
+            return false;
+        }
+        return Objects.equals(this.getId(), ((Product) obj).getId());
+    }
+
+    @Override
+    public int compareTo(Product o) {
+        return this.name.compareTo(o.getName());
+    }
+
+    @Override
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return null == this.id;
+    }
+
+}
