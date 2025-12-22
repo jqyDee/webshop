d4638826fc455861d2811651c4a530ce39d2c25c
Matti Sebastian Fischbach <matti.fischbach@student.uibk.ac.at>
2025-12-22 15:31:45 +0100

    feat: implemented cart item controller

---
+++ src/main/java/at/qe/skeleton/mappers/CartItemMapper.java
@@ -0,0 +1,11 @@
+package at.qe.skeleton.mappers;
+
+import at.qe.skeleton.dtos.CartItemDTO;
+import at.qe.skeleton.model.CartItem;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = {ProductMapper.class, UserxMapper.class})
+public abstract class CartItemMapper extends DTOMapper<CartItem, CartItemDTO>{
+    public abstract CartItemDTO mapTo(CartItem entity);
+    public abstract CartItem mapFrom(CartItemDTO dto);
+}
