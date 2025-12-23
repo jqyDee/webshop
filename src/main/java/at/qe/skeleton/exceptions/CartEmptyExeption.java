1790662b419e892153aeef282e50ba044a4b1757
Johanna Schaffer <johanna.schaffer@student.uibk.ac.at>
2025-12-24 00:02:18 +0100

    corrected semicolon

--- src/main/java/at/qe/skeleton/exceptions/CartEmptyExeption.java
+++ src/main/java/at/qe/skeleton/exceptions/CartEmptyExeption.java
@@ -2,6 +2,6 @@ package at.qe.skeleton.exceptions;
 
 public class CartEmptyExeption extends RuntimeException {
     public CartEmptyExeption() {
-        super("Can not create Order. Your Cart is empty.");;
+        super("Can not create Order. Your Cart is empty.");
     }
 }
