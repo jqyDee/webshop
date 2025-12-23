b065493b0227b24d5904c52651f3301b76ded5fd
Matti Sebastian Fischbach <matti.fischbach@student.uibk.ac.at>
2025-12-23 22:38:51 +0100

    feat: fixed reviews not being mapped correctly

--- src/main/java/at/qe/skeleton/mappers/ProductMapper.java
+++ src/main/java/at/qe/skeleton/mappers/ProductMapper.java
@@ -3,9 +3,11 @@ package at.qe.skeleton.mappers;
 import at.qe.skeleton.dtos.ProductDTO;
 import at.qe.skeleton.model.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public abstract class ProductMapper extends DTOMapper<Product, ProductDTO> {
     public abstract ProductDTO mapTo(Product entity);
+    @Mapping(target = "reviews", ignore = true)
     public abstract Product mapFrom(ProductDTO dto);
 }
