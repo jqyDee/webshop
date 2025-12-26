3e48cfae6cf6ae9a196f24dd2d83730ba4981686
Matti Sebastian Fischbach <matti.fischbach@student.uibk.ac.at>
2025-12-26 14:54:56 +0100

    feat: merge fixes

--- src/main/java/at/qe/skeleton/dtos/AddressDTO.java
+++ src/main/java/at/qe/skeleton/dtos/AddressDTO.java
@@ -1,8 +1,10 @@
 package at.qe.skeleton.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record AddressDTO(
+        @NotNull Long id,
         @NotBlank String street,
         @NotBlank String number,
         @NotBlank String postalCode,
