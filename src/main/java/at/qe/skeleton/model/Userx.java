8b2b4749251638364ac8b650a97edf0a1a61d382
Matti Sebastian Fischbach <matti.fischbach@student.uibk.ac.at>
2025-12-27 23:34:48 +0100

    feat: format correction

--- src/main/java/at/qe/skeleton/model/Userx.java
+++ src/main/java/at/qe/skeleton/model/Userx.java
@@ -23,208 +23,208 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Entity
 public class Userx implements Persistable<Long>, Serializable, Comparable<Userx>, UserDetails {
 
-  @Serial
-  private static final long serialVersionUID = 1L;
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  private Address shippingAddress;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Address shippingAddress;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  private Address paymentAddress;
-  
-  @ManyToOne(fetch = FetchType.LAZY)
-  private Userx createUser;
-  @Column(nullable = false)
-  @CreationTimestamp
-  private LocalDateTime createdDate;
-  @ManyToOne
-  private Userx updateUser;
-  @UpdateTimestamp
-  private LocalDateTime updatedDate;
-  
-  @Column(unique = true, nullable = false, length = 100)
-  private String username;
-  private String password;
-  
-  private String firstName;
-  private String lastName;
-  private String email;
-  private String phone;
-  
-  @ElementCollection(targetClass = UserxRole.class, fetch = FetchType.EAGER)
-  @CollectionTable(name = "Userx_UserxRole")
-  @Enumerated(EnumType.STRING)
-  private Set<UserxRole> roles;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Address paymentAddress;
 
-  boolean enabled;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Userx createUser;
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+    @ManyToOne
+    private Userx updateUser;
+    @UpdateTimestamp
+    private LocalDateTime updatedDate;
 
-  public Address getShippingAddress() {
-    return shippingAddress;
-  }
-  public void setShippingAddress(Address deliveryAddress) {this.shippingAddress = deliveryAddress;}
+    @Column(unique = true, nullable = false, length = 100)
+    private String username;
+    private String password;
 
-  public Address getPaymentAddress() {return paymentAddress;}
-  public void setPaymentAddress(Address PaymentAddress) {this.paymentAddress = PaymentAddress;}
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
 
-  @Override
-  public boolean isAccountNonExpired() {
-    return UserDetails.super.isAccountNonExpired();
-  }
+    @ElementCollection(targetClass = UserxRole.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "Userx_UserxRole")
+    @Enumerated(EnumType.STRING)
+    private Set<UserxRole> roles;
 
-  @Override
-  public boolean isAccountNonLocked() {
-    return UserDetails.super.isAccountNonLocked();
-  }
+    boolean enabled;
 
-  @Override
-  public boolean isCredentialsNonExpired() {
-    return UserDetails.super.isCredentialsNonExpired();
-  }
-
-  @Override
-  public Collection<? extends GrantedAuthority> getAuthorities() {
-    return getRoles();
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPhone() {
-    return phone;
-  }
-
-  public void setPhone(String phone) {
-    this.phone = phone;
-  }
-
-  public boolean isEnabled() {
-    return enabled;
-  }
-
-  public void setEnabled(boolean enabled) {
-    this.enabled = enabled;
-  }
-
-  public Set<UserxRole> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(Set<UserxRole> roles) {
-    this.roles = roles;
-  }
-
-  public Userx getCreateUser() {
-    return createUser;
-  }
-
-  public void setCreateUser(Userx createUser) {
-    this.createUser = createUser;
-  }
-
-  public LocalDateTime getCreatedDate() {
-    return createdDate;
-  }
-
-  public void setCreatedDate(LocalDateTime createDate) {
-    this.createdDate = createDate;
-  }
-
-  public Userx getUpdateUser() {
-    return updateUser;
-  }
-
-  public void setUpdateUser(Userx updateUser) {
-    this.updateUser = updateUser;
-  }
-
-  public LocalDateTime getUpdatedDate() {
-    return updatedDate;
-  }
-
-  public void setUpdatedDate(LocalDateTime updateDate) {
-    this.updatedDate = updateDate;
-  }
-
-  @Override
-  public int hashCode() {
-    int hash = 7;
-    hash = 59 * hash + Objects.hashCode(this.getId());
-    return hash;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj == null) {
-      return false;
+    public Address getShippingAddress() {
+        return shippingAddress;
     }
-    if (!(obj instanceof Userx other)) {
-      return false;
+    public void setShippingAddress(Address deliveryAddress) {this.shippingAddress = deliveryAddress;}
+
+    public Address getPaymentAddress() {return paymentAddress;}
+    public void setPaymentAddress(Address PaymentAddress) {this.paymentAddress = PaymentAddress;}
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
     }
-      return Objects.equals(this.getId(), other.getId());
-  }
 
-  @Override
-  public String toString() {
-    return "at.qe.skeleton.model.User[ id=" + username + " ]";
-  }
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
 
-  @Override
-  public Long getId() {
-    return id;
-  }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getRoles();
+    }
 
-  @Override
-  public boolean isNew() {
-    return (null == id);
-  }
+    public String getUsername() {
+        return username;
+    }
 
-  @Override
-  public int compareTo(Userx o) {
-    return this.id.compareTo(o.getId());
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Set<UserxRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<UserxRole> roles) {
+        this.roles = roles;
+    }
+
+    public Userx getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Userx createUser) {
+        this.createUser = createUser;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createDate) {
+        this.createdDate = createDate;
+    }
+
+    public Userx getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(Userx updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updateDate) {
+        this.updatedDate = updateDate;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + Objects.hashCode(this.getId());
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Userx other)) {
+            return false;
+        }
+        return Objects.equals(this.getId(), other.getId());
+    }
+
+    @Override
+    public String toString() {
+        return "at.qe.skeleton.model.User[ id=" + username + " ]";
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return (null == id);
+    }
+
+    @Override
+    public int compareTo(Userx o) {
+        return this.id.compareTo(o.getId());
+    }
 
 }
