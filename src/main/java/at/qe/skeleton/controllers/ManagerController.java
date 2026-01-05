a85050e38053344ac78822801e1fb5adcfe6a0c0
paulkretz <paul.kretz159@gmail.com>
2026-01-05 14:44:59 +0100

    feat: deleteProduct

--- src/main/java/at/qe/skeleton/controllers/ManagerController.java
+++ src/main/java/at/qe/skeleton/controllers/ManagerController.java
@@ -6,11 +6,15 @@ import at.qe.skeleton.dtos.ProductDTO;
 import at.qe.skeleton.mappers.ProductMapper;
 import at.qe.skeleton.model.Product;
 import at.qe.skeleton.services.ProductService;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/manager")
@@ -24,7 +28,12 @@ public class ManagerController {
         this.productMapper = productMapper;
     }
 
-
+    /**
+     * Creates a product
+     *
+     * @param productDto the user tb created
+     * @return {@link ResponseEntity} with status {@code 201 (Created)} with the newly created product in the body
+     */
     @PostMapping("/createProduct")
     public ResponseEntity<ProductDTO> createProduct(@Valid @RequestBody ProductDTO productDto) {
         Product product = productService.saveProduct(productMapper.mapFrom(productDto));
@@ -36,9 +45,19 @@ public class ManagerController {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * Deletes product of given id.
+     *
+     * @param id the id of the product tb deleted
+     * @return {@link ResponseEntity} with status {@code 204 (No Content)} on successful delete, or with status {@code 404 (Not Found)} if no product with this id exists
+     */
+
     @DeleteMapping("/product/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Product existingProduct = productService.loadProduct(id).orElseThrow(EntityNotFoundException::new);
+
+        productService.deleteProduct(existingProduct);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/orders")
