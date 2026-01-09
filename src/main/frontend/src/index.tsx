c3e3768692e212c89fb8e2529f72d727f0a02bce
jqydee <matti.fischbach@web.de>
2026-01-09 19:41:35 +0100

    feat: added prime flex to use the example layout from primereact

--- src/main/frontend/src/index.tsx
+++ src/main/frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'primeflex/primeflex.css'
 import App from './App';
 import {reportWebVitals} from "./reportWebVitals.ts";
 
