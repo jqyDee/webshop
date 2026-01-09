a10878db12304926b1221b441cdacdbf5b42d251
jqydee <matti.fischbach@web.de>
2026-01-09 19:42:27 +0100

    feat: product list component

---
+++ src/main/frontend/src/components/ProductTableComponent.tsx
@@ -0,0 +1,104 @@
+import ProductListComponent from "./ProductListComponent.tsx";
+import {useQuery} from "@tanstack/react-query";
+import {getProductsOptions} from "../api/@tanstack/react-query.gen.ts";
+import {useState} from "react";
+import {ProductFilterDto} from "../api";
+import {DataViewPageEvent} from "primereact/dataview";
+
+const ProductTableComponent = () => {
+    // pagination state
+    const [lazyState, setLazyState] = useState({
+        first: 0,
+        pageSize: 10,
+        pageId: 0,
+    });
+
+    // sorting
+    const [sortKey, setSortKey] = useState<string>('name');
+    const [sortField, setSortField] = useState<string>('name');
+    const [sortOrder, setSortOrder] = useState<number>(1); // 1 = Asc, -1 = Desc
+
+    const sortOptions = [
+        { label: 'Name: A-Z', value: 'name' },
+        { label: 'Price High to Low', value: '!price' },
+        { label: 'Price Low to High', value: 'price' },
+        { label: 'Rating: High to Low', value: '!rating' },
+    ];
+
+    const onSortChange = (event: any) => {
+        const value = event.value;
+
+        if (value.indexOf('!') === 0) {
+            setSortOrder(-1);
+            setSortField(value.substring(1));
+            setSortKey(value);
+        } else {
+            setSortOrder(1);
+            setSortField(value);
+            setSortKey(value);
+        }
+
+        // reset pagination to first page
+        setLazyState(prev => ({ ...prev, first: 0, pageId: 0 }));
+    }
+
+    // filters
+    const [filters, setFilters] = useState<ProductFilterDto>({
+        name: '',
+        minPrice: undefined,
+        maxPrice: undefined,
+        minRating: undefined,
+        minStock: undefined,
+    });
+
+    const onNameChange= (value: string) => {
+        setFilters(prev => ({ ...prev, name: value }));
+        setLazyState(prev => ({ ...prev, first: 0, pageId: 0 }));
+    };
+
+    const onFilterChange = (field: keyof ProductFilterDto, value: any) => {
+        setFilters(prev => ({ ...prev, [field]: value }));
+        // reset pagination to first page
+        setLazyState(prev => ({ ...prev, first: 0, pageId: 0 }));
+    }
+
+    // query
+    const {data: pageData, isLoading} = useQuery({
+        ...getProductsOptions({
+            query: {
+                pageId: lazyState.pageId,
+                pageSize: lazyState.pageSize,
+                sort: [`${sortField},${sortOrder === 1 ? 'asc' : 'desc'}`],
+                ...filters,
+            }
+        }),
+    });
+
+    // DataView Page Event
+    const onPage = (event: DataViewPageEvent) => {
+        setLazyState({
+            first: event.first,
+            pageSize: event.rows,
+            pageId: event.page ?? 0
+        });
+    };
+
+    return (
+        <ProductListComponent
+            products={pageData?.items ?? []}
+            totalCount={pageData?.totalCount ?? 0}
+            loading={isLoading}
+            pageSize={lazyState.pageSize}
+            first={lazyState.first}
+            onPage={onPage}
+            filters={filters}
+            onFilterChange={onFilterChange}
+            onNameChange={onNameChange}
+            sortOptions={sortOptions}
+            sortKey={sortKey}
+            onSortChange={onSortChange}
+        />
+    )
+}
+
+export default ProductTableComponent;
--- src/main/frontend/src/views/HomePage.tsx
+++ src/main/frontend/src/views/HomePage.tsx
@@ -2,13 +2,11 @@
  * This code is part of the skeleton project provided for students of the course "Software
  * Architecture" offered by Innsbruck University.
  */
-import logo from '../logo.svg';
 import '../styles/App.css';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-import {Message} from 'primereact/message';
 import React from "react";
 import NavbarComponent from "../components/NavbarComponent";
-import {FooterComponent} from "../components/FooterComponent";
+import ProductTableComponent from "../components/ProductTableComponent.tsx";
 
 /**
  * The home page of the application.
@@ -18,25 +16,7 @@ class HomePage extends React.Component {
         return (
             <div>
                 <NavbarComponent />
-                <div className="App">
-                    <header className="App-header">
-                        <img src={logo} className="App-logo" alt="logo" />
-                        <p>
-                            Welcome to the SWA Skeleton Project!
-                        </p>
-                        <a
-                            className="App-link"
-                            href="https://reactjs.org"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Learn React
-                        </a>
-                        <span style={{ paddingTop: '20px' }} />
-                        <Message severity={"success"} text={"PrimeReact is installed!"} />
-                    </header>
-                </div>
-                <FooterComponent/>
+                <ProductTableComponent />
             </div>
         );
     }
