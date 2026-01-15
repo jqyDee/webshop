cb801f83110fa36c9616b8c307f42c0a786adb60
Johanna Schaffer <johanna.schaffer@student.uibk.ac.at>
2026-01-15 22:04:52 +0100

    feat: OrderDetails Component

--- src/main/frontend/src/components/OrderDetailsComponent.tsx
+++ src/main/frontend/src/components/OrderDetailsComponent.tsx
@@ -1,9 +1,11 @@
 import {useParams} from "react-router-dom";
 import {useQuery} from "@tanstack/react-query";
-import {getProductByIdOptions} from "../api/@tanstack/react-query.gen.ts";
 import {ProgressSpinner} from "primereact/progressspinner";
 import React, {useRef} from "react";
 import {Toast} from "primereact/toast";
+import {Tag} from "primereact/tag";
+import {DataTable} from "primereact/datatable";
+import {Column} from "primereact/column";
 
 const OrderDetailsComponent: React.FC = () => {
 
@@ -31,10 +33,32 @@ const OrderDetailsComponent: React.FC = () => {
     );
 
     return (
-        <div className="border-none">
-            <Toast ref={toast}/>
+        <div className="surface-card p-4 shadow-2 border-round">
+            <div className="flex justify-content-between align-items-center mb-5">
+                <h2 className="m-0">Orderdetails #{id}</h2>
+                <Tag value={order.status} severity={getStatusSeverity(order.status)} />
+            </div>
 
+            <DataTable value={order.poducts} responsiveLayout="stack" breakpoint="960px">
+                <Column field="name" header="Productname" fontStyle="bold" />
+                <Column field="quantity" header="Quantity" />
+                <Column
+                    header="Price"
+                    body={(item) => `${(item.total).toFixed(2)} €`}
+                />
+                <Column
+                    field="total"
+                    header="Summe"
+                    body={(item) => <span className="font-bold text-primary">{item.total * item.quantity} €</span>}
+                />
+            </DataTable>
 
+            <div className="flex justify-content-end mt-4">
+                <div className="text-right">
+                    <span className="text-xl font-light block mb-2">Gesamtsumme</span>
+                    <span className="text-4xl font-bold text-900">{order.sum} €</span>
+                </div>
+            </div>
         </div>
     );
 }
