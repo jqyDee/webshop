fa1b72ea9c63e5ee3d89cd4540af95c0d7393708
Tim Janusch <tim.janusch@student.uibk.ac.at>
2026-01-29 22:21:47 +0100

    feat: add notify options; fix: bad auth error

--- src/main/frontend/src/components/user-dialog.tsx
+++ src/main/frontend/src/components/user-dialog.tsx
@@ -1,5 +1,5 @@
 import React, {forwardRef, useImperativeHandle, useMemo, useState} from "react";
-import {RoleEnum, UserxDto, UserxUpdateDto} from "../api";
+import {ItemsEnum, RoleEnum, UserxDto, UserxUpdateDto} from "../api";
 import {InputMaskChangeEvent} from "primereact/inputmask";
 import {CheckboxChangeEvent} from "primereact/checkbox";
 import {QueryObserverResult, RefetchOptions, useMutation} from "@tanstack/react-query";
@@ -130,7 +130,6 @@ export const UserDialog = forwardRef<UserDialogHandle, UserDialogComponentProps>
             setValidation({valid: true});
 
             if (isRegister) {
-                console.log('here')
                 // This has to be there, see validation
                 const pw = selectedUser.password!;
 
@@ -155,7 +154,7 @@ export const UserDialog = forwardRef<UserDialogHandle, UserDialogComponentProps>
          */
         const openEditDialog = (user: UserxDto) => {
             setSubmitting(false);
-            setSelectedUser(fromUserxDtoToUserxUpdateDto(user));
+            setSelectedUser(user);
             setValidation({valid: true});
             setIsNewUser(false);
             showDialog()
@@ -229,6 +228,17 @@ export const UserDialog = forwardRef<UserDialogHandle, UserDialogComponentProps>
             setSelectedUser({...selectedUser, role: role});
         }
 
+        const handleNotifyOptionToggle = (option: ItemsEnum) => {
+            if (!selectedUser) return;
+            const currentOptions = selectedUser.notifyOptions ?? [];
+            const isPresent = currentOptions.find(opt => option === opt);
+            if (isPresent) {
+                setSelectedUser({...selectedUser, notifyOptions: currentOptions.filter(opt => opt === option)});
+                return;
+            }
+            setSelectedUser({...selectedUser, notifyOptions: [...currentOptions, option]});
+        }
+
         /**
          * Renders the footer of the dialog.
          */
@@ -262,6 +272,7 @@ export const UserDialog = forwardRef<UserDialogHandle, UserDialogComponentProps>
                             canSetRole={canSetRole}
                             onInputChange={handleInputChange}
                             onRolesChange={handleRolesChange}
+                            onToggleNotifyOption={handleNotifyOptionToggle}
                             onUserEnabledChange={handleUserEnabledChange}
                         />
                     )}
@@ -290,25 +301,7 @@ const createUserxRoleArrayFromStrings = (role: string): RoleEnum => {
             throw new Error(`Invalid role: ${role}`);
     }
 }
-/**
- * Create UserxUpdateDto from UserxDto
- * @param user UserxDto to map from
- *
- * @returns UserxUpdateDto
- */
-const fromUserxDtoToUserxUpdateDto = (user: UserxDto): UserxUpdateDto => {
-    return {
-        id: user.id,
-        username: user.username,
-        password: "",
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        phone: user.phone,
-        enabled: user.enabled,
-        role: user.role,
-    };
-}
+
 /**
  * Create empty UserxUpdateDto for new User Dialog
  *
